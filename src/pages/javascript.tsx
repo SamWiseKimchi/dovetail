27f478868e89745a57ec634647d630f0163a14b5
Sam Kim <seyeunkim@hotmail.com>
2023-09-01 16:54:53 +1000

    implement components into entry point'

--- src/pages/javascript.tsx
+++ src/pages/javascript.tsx
@@ -1,11 +1,29 @@
 // If you don't want to use TypeScript you can delete this file!
 import * as React from "react"
 import Layout from "../components/layout"
+import { type ContentInterface, content } from "../constants/content"
+import GridItem from "../components/GridItem"
+import Modal from "../components/Modal"
+import * as style from "./style.module.css"
 
-const JavascriptPage: React.FC = () => (
-  <Layout pageTitle="Javascript page">
-    <h1>Your solution will go here!</h1>
-  </Layout>
-)
+const JavascriptPage: React.FC = () => {
+  const [modalContent, setModalContent] =
+    React.useState<ContentInterface | null>(null)
+
+  return (
+    <Layout pageTitle="Javascript page">
+      <div className={style.container}>
+        <h1 className={style.title}>Latest news</h1>
+        <div className={style.grid}>
+          {content.map(c => {
+            return <GridItem content={c} onClick={() => setModalContent(c)} />
+          })}
+        </div>
+      </div>
+      {modalContent && (
+        <Modal content={modalContent} onClose={() => setModalContent(null)} />
+      )}
+    </Layout>
+  )
+}
 export default JavascriptPage
-
